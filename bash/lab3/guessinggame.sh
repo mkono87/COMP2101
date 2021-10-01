bebbd6eb1c2979eea94890bf12d0a98377fba07f
mkono87 <mkonowaluk@gmail.com>
2021-10-01 18:42:45 -0400

    Finished lab 3 practice

---
+++ bash/lab3/guessinggame.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# This script implements a guessing game
+# it will pick a secret number from 1 to 10
+# it will then repeatedly ask the user to guess the number
+#    until the user gets it right
+
+# Give the user instructions for the game
+cat <<EOF
+Let's play a game.
+I will pick a secret number from 1 to 10 and you have to guess it.
+If you get it right, you get a virtual prize.
+Here we go!
+
+EOF
+
+# Pick the secret number and save it
+secretnumber=$(($RANDOM % 10 +1)) # save our secret number to compare later
+
+# This loop repeatedly asks the user to guess and tells them if they got the right answer
+# TASK 1: Test the user input to make sure it is not blank
+# TASK 2: Test the user input to make sure it is a number from 1 to 10 inclusive
+# TASK 3: Tell the user if their guess is too low, or too high after each incorrect guess
+
+while [ $userguess != $secretnumber ]; do # ask repeatedly until they get it right
+  read -p "Give me a number from 1 to 10: " userguess # ask for another guess
+if $userguess == ""; then
+  echo "Number cannot be blank"
+elif $userguess >10 -a < 1; then
+  echo "Number must be between 1 and 10"
+fi
+done
+echo "You got it! Have a milkdud."
+
