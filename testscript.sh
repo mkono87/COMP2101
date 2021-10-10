02c2cb0fa353d8f0b91cda2435e3e24cb20a86a1
mkono87 <mkonowaluk@gmail.com>
2021-10-10 10:59:52 -0400

    worked on lab 4

--- testscript.sh
+++ testscript.sh
@@ -1,10 +1,33 @@
-interfaceList=$(ip -o link show | awk -F': ' '{print $2}')
-ifArray=()
-for i in $interfaceList; do
-if [ $i != "lo" ]; then
+# interfaceList=$(ip -o link show | awk -F': ' '{print $2}')
+# ifArray=()
+# for i in $interfaceList; do
+# if [ $i != "lo" ]; then
+# echo $i
+# fi
+# done
 
-ifArray+=$i
-fi
+
+interface="ens33"
+while [ $# -gt 0 ]; do
+  case "$1" in
+  -v | --verbose )
+    verbose="yes"
+    echo "Verbose logging turned on"
+    ;;
+  -* )
+    echo "Unknown argument, exiting"
+    exit
+    ;;
+  # Any string not starting with -
+  *)
+    # Check if we already set an interface
+    if [ -n "$interface" ]
+    then
+      echo >&2 "Only one interface is allowed"
+      exit 1
+    fi
+    interface="$1"
+    ;;
+  esac
+  shift
 done
-
-echo $i
