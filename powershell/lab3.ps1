3dd36da3563c001a9daec763fb73147d5e7c8b7c
mkono87 <mkonowaluk@gmail.com>
2021-11-17 17:37:16 -0500

    Update lab3.ps1

--- powershell/lab3.ps1
+++ powershell/lab3.ps1
@@ -1,4 +1,4 @@
 ﻿get-ciminstance win32_networkadapterconfiguration |
   Where-Object {$_.IPEnabled -eq 'true'} |
-    format-table Description,index,IPAddress,DNSDomain,DNSServer |
+    format-table Description,index,IPAddress,DNSDomain,DNSHostName |
       more
