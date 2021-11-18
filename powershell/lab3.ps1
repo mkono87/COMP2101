55ec2edf7df6b9c6ba82058634f9c2ae8616b868
mkono87 <mkonowaluk@gmail.com>
2021-11-17 21:52:04 -0500

    Update lab3.ps1

--- powershell/lab3.ps1
+++ powershell/lab3.ps1
@@ -1,4 +1,8 @@
 ﻿get-ciminstance win32_networkadapterconfiguration |
   Where-Object {$_.IPEnabled -eq 'true'} |
-    format-table Description,index,IPAddress,DNSDomain,DNSHostName |
-      more
+    Add-Member -MemberType AliasProperty -Name DNSServer -Value DNSServerSearchOrder -PassThru |
+      Add-Member -MemberType AliasProperty -Name SubnetMask -Value IPSubnet -PassThru |
+        Select-Object Description,index,IPAddress,SubnetMask,DNSServer,DNSDomain |
+          Format-table -AutoSize
+          
+
