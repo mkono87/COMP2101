604659c4ac425644261f125ffa9d8c8cb61e289e
mkono87 <mkonowaluk@gmail.com>
2021-11-25 22:14:44 -0500

    Update lab4.ps1

--- powershell/lab4.ps1
+++ powershell/lab4.ps1
@@ -1,22 +1,24 @@
-﻿$SystemInfo =  Get-CimInstance Win32_computersystem
+﻿
 $OsInfo = Get-CimInstance Win32_operatingsystem
 $CPUInfo = Get-CimInstance win32_processor
 
 function systeminfo{
-write-host ("System Information")
-$SystemInfo | fl Name,Model,Manufacturer,SystemType
+    $SystemInfo =  Get-CimInstance Win32_computersystem
+    write-host ("System Information")
+    $SystemInfo 
 }
+$SystemInfo
 
 function OS {
-write-host ("Operating System Details")
-$CustomOSInfo = [PSCustomObject]@{
-    'OS Edition' = $OsInfo.Caption
-    'Build Version' = $OsInfo.Version
-    'Installed On' = $OsInfo.InstallDate
-    'Architecture' = $OsInfo.OSArchitecture
-
+    $OsInfo = Get-CimInstance Win32_operatingsystem
+    write-host ("Operating System Details")
+    $CustomOSInfo = [PSCustomObject]@{
+        'OS Edition' = $OsInfo.Caption
+        'Build Version' = $OsInfo.Version
+        'Installed On' = $OsInfo.InstallDate
+        'Architecture' = $OsInfo.OSArchitecture
     }
-    $CustomOSInfo | fl
+$CustomOSInfo
 
 }
 
@@ -28,13 +30,27 @@ $CustomCPUInfo = [PsCustomObject] @{
     'L1 Cache Size' = $CPUInfo.L1CacheSize
     'L2 Cache Size' = $CPUInfo.L2CacheSize
     'L3 Cache Size' = $CPUInfo.L3CacheSize
-    }|
-    sssss
-   
+    }
+$CustomCPUInfo
 }
 
 
 
-systeminfo
-Os
-CPU
+systeminfo | Format-List Name,Model,Manufacturer,SystemType
+Os | Format-List 
+CPU | Format-List
+
+
+
+cputest = @{
+    'Model' = $CPUInfo.Name
+    'Cores' = $CPUInfo.NumberofCores
+    'L1 Cache Size' = $CPUInfo.L1CacheSize
+    'L2 Cache Size' = $CPUInfo.L2CacheSize
+    'L3 Cache Size' = $CPUInfo.L3CacheSize
+}
+Foreach ($value in $cputest){
+    if ($null -eq $value){
+        
+    }
+}
