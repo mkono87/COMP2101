c2d2989ad6dda9403bbd63280736ab247710cdd8
mkono87 <35116314+mkono87@users.noreply.github.com>
2021-11-27 13:23:08 -0500

    Update to lab4

--- powershell/lab4.ps1
+++ powershell/lab4.ps1
@@ -1,16 +1,17 @@
 ﻿
-$OsInfo = Get-CimInstance Win32_operatingsystem
-$CPUInfo = Get-CimInstance win32_processor
 
+
+# Generic System Information
 function systeminfo{
-    $SystemInfo =  Get-CimInstance Win32_computersystem
+    $SystemInfo =  Get-WmiObject Win32_computersystem
     write-host ("System Information")
     $SystemInfo 
 }
-$SystemInfo
+systeminfo | Format-List
 
+# Operating System Information
 function OS {
-    $OsInfo = Get-CimInstance Win32_operatingsystem
+    $OsInfo = Get-WMIobject -Class Win32_operatingsystem
     write-host ("Operating System Details")
     $CustomOSInfo = [PSCustomObject]@{
         'OS Edition' = $OsInfo.Caption
@@ -21,36 +22,62 @@ function OS {
 $CustomOSInfo
 
 }
-
+# Processor Information
 function CPU {
 write-host ("Processor Information")
-$CustomCPUInfo = [PsCustomObject] @{
-    'Model' = $CPUInfo.Name
-    'Cores' = $CPUInfo.NumberofCores
-    'L1 Cache Size' = $CPUInfo.L1CacheSize
-    'L2 Cache Size' = $CPUInfo.L2CacheSize
-    'L3 Cache Size' = $CPUInfo.L3CacheSize
-    }
-$CustomCPUInfo
-}
+Get-CimInstance win32_processor | 
+foreach {
+    new-object -TypeName psobject -Property @{
+        L1Cache= if ($_.L1CacheSize) {$_.L1CacheSize}
+        else {"Not Available"}       
+        L2Cache= if ($_.L2CacheSize) {$_.L2CacheSize}
+        else {"Not Available"}
+        L3Cache= if ($_.L3CacheSize) {$_.L3CacheSize}
+        else {"Not Available"}
+
+    }}| Format-List Name,NumberOfCores,L1Cache,L2Cache,L3Cache
+} 
 
 
 
-systeminfo | Format-List Name,Model,Manufacturer,SystemType
-Os | Format-List 
-CPU | Format-List
+CPU
 
 
 
-cputest = @{
-    'Model' = $CPUInfo.Name
-    'Cores' = $CPUInfo.NumberofCores
-    'L1 Cache Size' = $CPUInfo.L1CacheSize
-    'L2 Cache Size' = $CPUInfo.L2CacheSize
-    'L3 Cache Size' = $CPUInfo.L3CacheSize
-}
-Foreach ($value in $cputest){
-    if ($null -eq $value){
-        
-    }
-}
+#Memory Interformation
+
+$totalcapacity = 0 
+Write-host ("Memory Information")
+get-wmiobject -class win32_physicalmemory |  
+foreach { 
+    new-object -TypeName psobject -Property @{ 
+                Manufacturer = $_.manufacturer 
+                "Speed(MHz)" = $_.speed 
+                "Size(MB)" = $_.capacity/1gb 
+                Bank = $_.banklabel 
+                Slot = $_.devicelocator 
+    } 
+    $totalcapacity += $_.capacity/1gb
+
+} | 
+ft -auto Manufacturer, "Size(MB)", "Speed(MHz)", Bank, Slot 
+"Total RAM: ${totalcapacity}GB "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# systeminfo | Format-List Name,Model,Manufacturer,SystemType
+# Os | Format-List
+
+
+
